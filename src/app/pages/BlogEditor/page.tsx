158775f31f3306ddd7028ec3a80d511c06c46c1d
cotychunsik <toboldygo@naver.com>
2024-10-04 14:55:48 +0900

    충돌좀그만하라고

--- src/app/pages/BlogEditor/page.tsx
+++ src/app/pages/BlogEditor/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import dynamicImport from 'next/dynamic'; // 이름을 'dynamicImport'로 변경하여 충돌 방지
 import { useSearchParams } from 'next/navigation';
 import { ref, get, child, set } from 'firebase/database';
 import { database } from '../../../../firebaseConfig';
 
+// Quill 에디터를 동적으로 로드 (SSR을 방지)
+const ReactQuill = dynamicImport(() => import('react-quill'), { ssr: false });
+
+export const dynamicRendering = 'force-dynamic'; // 동적 렌더링 강제
+
 export default function BlogEditor() {
-  const searchParams = useSearchParams(); // 이 훅을 클라이언트 컴포넌트 최상단에서 사용
+  const searchParams = useSearchParams();
   const postIndex = searchParams.get('postIndex');
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
@@ -52,10 +58,10 @@ export default function BlogEditor() {
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <textarea
-        placeholder="내용을 입력하세요"
+      <ReactQuill
         value={content}
-        onChange={(e) => setContent(e.target.value)}
+        onChange={setContent}
+        theme="snow"
       />
       <button onClick={handleSavePost}>
         {postIndex !== null ? '수정하기' : '게시하기'}
