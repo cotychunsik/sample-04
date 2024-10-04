cc0ed3a78418701e30ed38765101ba7c3aa551a3
cotychunsik <toboldygo@naver.com>
2024-10-04 14:28:24 +0900

    config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 동적 렌더링 강제 (모든 페이지)
+  experimental: {
+    runtime: 'experimental-edge',  // Edge Runtime 사용 (필요시)
+  },
+  reactStrictMode: true, // React의 엄격 모드
+  swcMinify: true, // 성능 최적화를 위한 Minify 옵션
+};
 
 export default nextConfig;
